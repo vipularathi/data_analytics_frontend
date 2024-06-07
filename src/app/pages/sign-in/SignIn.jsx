a13c2fe581d330ed73108f6f3e7542a590cfdc29
COLO <vrushabhchougule@rathi.com>
2024-06-07 17:20:29 +0530

    Changed the chart to scatter and structure of table

--- src/app/pages/sign-in/SignIn.jsx
+++ src/app/pages/sign-in/SignIn.jsx
@@ -1,19 +1,187 @@
+// import { Link, useNavigate } from "@tanstack/react-router";
+// import { useState } from "react";
+// import { observer } from "mobx-react-lite";
+// import { useAuth } from "../../hooks/store/use-auth";
+// import { authApi } from "../../services/auth.service";
+ 
+// const SignInPage = observer(() => {
+//   const authStore = useAuth();
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [otp, setOtp] = useState("");
+//   const [errMsg, setErrMsg] = useState(null);
+//   const [message, setMessage] = useState(false);
+//   const navigate = useNavigate();
+ 
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setErrMsg(null);
+//     const res = await authApi.signIn(email, password);
+//     console.log(res);
+ 
+//     if (res.output === "login success") {
+//       setMessage(false);
+//       navigate({ to: "/analytics/custom-chart" });
+//     } else {
+//       setMessage(true);
+//       navigate({ to: "/signin" });
+//     }
+//   };
+ 
+//   const handleOtpSubmit = (e) => {
+//     e.preventDefault();
+//     authStore
+//       .verifyOtp({ otp })
+//       .then((resp) => {
+//         if ("token" in resp) {
+//           navigate({ to: "/admin" });
+//         }
+//       })
+//       .catch((error) => {
+//         setErrMsg(error.message);
+//       })
+//       .finally(() => {
+//         setOtp("");
+//       });
+//   };
+ 
+//   return (
+//     <div
+//       style={{
+//         display: "flex",
+//         justifyContent: "center",
+//         alignItems: "center",
+//         height: "100vh",
+//         marginLeft: "40%",
+//       }}
+//     >
+//       <div
+//         style={{
+//           width: "300px",
+//           padding: "20px",
+//           border: "1px solid #ccc",
+//           borderRadius: "5px",
+//           boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
+//         }}
+//       >
+//         <h3>Login page</h3>
+//         {/* <Link to="/signup">Sign Up</Link>
+//         <Link to="/forget-password">Forget Password</Link> */}
+//         <form onSubmit={handleSubmit}>
+//           <fieldset>
+//             <div
+//               style={{
+//                 padding: 10,
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 gap: 10,
+//               }}
+//             >
+//               <label htmlFor="username-input">
+//                 Email
+//                 <input
+//                   id="username-input"
+//                   type="email"
+//                   value={email}
+//                   onChange={(e) => setEmail(e.target.value)}
+//                   required
+//                 />
+//               </label>
+//               <label htmlFor="password">
+//                 Password
+//                 <input
+//                   id="password"
+//                   type="password"
+//                   value={password}
+//                   onChange={(e) => setPassword(e.target.value)}
+//                   required
+//                 />
+//               </label>
+//             </div>
+//             <button
+//               type="submit"
+//               style={{
+//                 width: "100%",
+//                 backgroundColor: "#007bff",
+//                 color: "#fff",
+//                 border: "none",
+//                 borderRadius: "4px",
+//                 padding: "10px 0",
+//                 cursor: "pointer",
+//               }}
+//             >
+//               {authStore.isLoading ? "Loading..." : "Login"}
+//             </button>
+//             <p style={{ textAlign: "center", color: "red" }}>{errMsg}</p>
+//           </fieldset>
+//         </form>
+ 
+//         {authStore.showOtp && (
+//           <form onSubmit={handleOtpSubmit}>
+//             <div
+//               style={{
+//                 padding: 10,
+//                 display: "flex",
+//                 flexDirection: "column",
+//                 gap: 10,
+//               }}
+//             >
+//               <label htmlFor="otp">
+//                 OTP
+//                 <input
+//                   id="otp"
+//                   type="number"
+//                   value={otp}
+//                   onChange={(e) => setOtp(e.target.value)}
+//                   required
+//                 />
+//               </label>
+//             </div>
+//             <button
+//               type="submit"
+//               style={{
+//                 width: "100%",
+//                 backgroundColor: "#007bff",
+//                 color: "#fff",
+//                 border: "none",
+//                 borderRadius: "4px",
+//                 padding: "10px 0",
+//                 cursor: "pointer",
+//               }}
+//             >
+//               {authStore.isLoading ? "Loading..." : "Verify"}
+//             </button>
+//             <p style={{ textAlign: "center", color: "red" }}>{errMsg}</p>
+//           </form>
+//         )}
+//       </div>
+//     </div>
+//   );
+// });
+ 
+// export default SignInPage;
+
 import { Link, useNavigate } from "@tanstack/react-router";
 import { useState } from "react";
 import { observer } from "mobx-react-lite";
 import { useAuth } from "../../hooks/store/use-auth";
 import { authApi } from "../../services/auth.service";
+import { TextField, Button, Typography, Box, Container,Card,CardContent } from '@mui/material';
+import { useTheme } from "@mui/material";
  
 const SignInPage = observer(() => {
+  const theme = useTheme();
   const authStore = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [otp, setOtp] = useState("");
-  const [message, setMessage] = useState(false);
   const [errMsg, setErrMsg] = useState(null);
+  const [message, setMessage] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
  
   const handleSubmit = async (e) => {
+ 
     e.preventDefault();
     setErrMsg(null);
     const res = await authApi.signIn(email, password);
@@ -44,178 +212,93 @@ const SignInPage = observer(() => {
         setOtp("");
       });
   };
+  console.log(theme.palette.mode)
+ 
   return (
-    <div>
-      <h3>Login page</h3>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/forget-password">Forget Password</Link>
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <div style={{ padding: 10, display: "flex", gap: 10 }}>
-            <label htmlFor="username-input">
-              Email
-              <input
+    <Container maxWidth="sm">
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Card sx={{ width: '300px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+        <CardContent>
+          <Typography variant="h5" align="center" gutterBottom>
+            Login page
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <div style={{ padding: 10, display: 'flex', flexDirection: 'column', gap: 10 }}>
+              <TextField
                 id="username-input"
+                label="Email"
                 type="email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
               />
-            </label>
-          </div>
-          <div style={{ padding: 10, display: "flex", gap: 10 }}>
-            <label htmlFor="password">
-              Password
-              <input
+              <TextField
                 id="password"
+                label="Password"
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
               />
-            </label>
-          </div>
+            </div>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              disabled={loading}
+              sx={{ mt: 2,color:"white" }}
+            >
+              {loading ? 'Loading...' : 'Login'}
+            </Button>
+            {errMsg && (
+              <Typography variant="body2" align="center" color="error" sx={{ mt: 1 }}>
+                {errMsg}
+              </Typography>
+            )}
+          </form>
  
-          <button type="submit">
-            {authStore.isLoading ? "Loading..." : "Login"}
-          </button>
-          {message ? <p style={{ color: "red" }}>Check credentials</p> : <> </>}
-          {/* <p>{errMsg}</p> */}
-        </fieldset>
-      </form>
- 
-      {authStore.showOtp && (
-        <form onSubmit={handleOtpSubmit}>
-          <div style={{ padding: 10, display: "flex", gap: 10 }}>
-            <label htmlFor="otp">
-              OTP
-              <input
-                id="otp"
-                type="number"
-                value={otp}
-                onChange={(e) => setOtp(e.target.value)}
-                required
-              />
-            </label>
-          </div>
-          <button type="submit">
-            {authStore.isLoading ? "Loading..." : "verify"}
-          </button>
-          <p>{errMsg}</p>
-        </form>
-      )}
-    </div>
+          {authStore.showOtp && (
+            <form onSubmit={handleOtpSubmit}>
+              <div style={{ padding: 10, display: 'flex', flexDirection: 'column', gap: 10 }}>
+                <TextField
+                  id="otp"
+                  label="OTP"
+                  type="number"
+                  value={otp}
+                  onChange={(e) => setOtp(e.target.value)}
+                  required
+                />
+              </div>
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                fullWidth
+                disabled={loading}
+                sx={{ mt: 2 }}
+              >
+                {loading ? 'Loading...' : 'Verify'}
+              </Button>
+              {errMsg && (
+                <Typography variant="body2" align="center" color="error" sx={{ mt: 1 }}>
+                  {errMsg}
+                </Typography>
+              )}
+            </form>
+          )}
+        </CardContent>
+      </Card>
+    </Box>
+  </Container>
   );
 });
  
 export default SignInPage;
- 
-//still to test
-// import { Link, useNavigate } from "@tanstack/react-router";
-// import { useState } from "react";
-// import { observer } from "mobx-react-lite";
-// import { useAuth } from "../../hooks/store/use-auth";
- 
-// const SignInPage = observer(() => {
-//   const authStore = useAuth();
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   const [otp, setOtp] = useState("");
-//   const [errMsg, setErrMsg] = useState(null);
-//   const navigate = useNavigate();
- 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setErrMsg(null);
-//     authStore
-//       .signIn({ provider: "email", email, password })
-//       .then((resp) => {
-//         if ("token" in resp) {
-//           navigate({ to: "/dashboard" });
-//         }
-//       })
-//       .catch((error) => {
-//         setErrMsg(error.message);
-//       });
-//   };
- 
-//   const handleOtpSubmit = (e) => {
-//     e.preventDefault();
-//     authStore
-//       .verifyOtp({ otp })
-//       .then((resp) => {
-//         if ("token" in resp) {
-//           navigate({ to: "/admin" });
-//         }
-//       })
-//       .catch((error) => {
-//         setErrMsg(error.message);
-//       })
-//       .finally(() => {
-//         setOtp("");
-//       });
-//   };
- 
-//   return (
-//     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh',marginLeft:"40%"}}>
-//       <div style={{ width: '300px', padding: '20px', border: '1px solid #ccc', borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
-//         <h3>Login page</h3>
-//         {/* <Link to="/signup">Sign Up</Link>
-//         <Link to="/forget-password">Forget Password</Link> */}
-//         <form onSubmit={handleSubmit}>
-//           <fieldset>
-//             <div style={{ padding: 10, display: "flex", flexDirection: "column", gap: 10 }}>
-//               <label htmlFor="username-input">
-//                 Email
-//                 <input
-//                   id="username-input"
-//                   type="email"
-//                   value={email}
-//                   onChange={(e) => setEmail(e.target.value)}
-//                   required
-//                 />
-//               </label>
-//               <label htmlFor="password">
-//                 Password
-//                 <input
-//                   id="password"
-//                   type="password"
-//                   value={password}
-//                   onChange={(e) => setPassword(e.target.value)}
-//                   required
-//                 />
-//               </label>
-//             </div>
-//             <button type="submit" style={{ width: '100%', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', padding: '10px 0', cursor: 'pointer' }}>
-//               {authStore.isLoading ? "Loading..." : "Login"}
-//             </button>
-//             <p style={{ textAlign: "center", color: "red" }}>{errMsg}</p>
-//           </fieldset>
-//         </form>
- 
-//         {authStore.showOtp && (
-//           <form onSubmit={handleOtpSubmit}>
-//             <div style={{ padding: 10, display: "flex", flexDirection: "column", gap: 10 }}>
-//               <label htmlFor="otp">
-//                 OTP
-//                 <input
-//                   id="otp"
-//                   type="number"
-//                   value={otp}
-//                   onChange={(e) => setOtp(e.target.value)}
-//                   required
-//                 />
-//               </label>
-//             </div>
-//             <button type="submit" style={{ width: '100%', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', padding: '10px 0', cursor: 'pointer' }}>
-//               {authStore.isLoading ? "Loading..." : "Verify"}
-//             </button>
-//             <p style={{ textAlign: "center", color: "red" }}>{errMsg}</p>
-//           </form>
-//         )}
-//       </div>
-//     </div>
-//   );
-// });
- 
-// export default SignInPage;
--- src/app/services/auth.service.js
+++ src/app/services/auth.service.js
@@ -1,63 +1,63 @@
 import { API_BASE_URL } from "../utils/url";
 import { BaseApi } from "./base.service";
-
+ 
 // Auth service
 class AuthApi extends BaseApi {
   authConfig = {
-    signInUrl: "auth/sign-in",
+    signInUrl: "/login",
     signUpUrl: "auth/sign-up",
     sendOtpUrl: "auth/otp",
     signOutUrl: "auth/logout",
     verifyOtpUrl: "auth/verify-otp",
     verifyTokenUrl: "auth/verify-token",
   };
-
+ 
   constructor() {
     super(API_BASE_URL);
   }
-
+ 
   getToken() {
     return localStorage.getItem("token");
   }
-
+ 
   setToken(token) {
     localStorage.setItem("token", token);
     this.axiosInstance.defaults.headers.common["auth-token"] = token;
   }
-
+ 
   setSession(token) {
     this.axiosInstance.defaults.headers.common["auth-token"] = token;
   }
-
+ 
   removeToken() {
     localStorage.removeItem("token");
     delete this.axiosInstance.defaults.headers.common["auth-token"];
   }
-
+ 
   sendOtp(data) {
     return this.axiosInstance.post(this.authConfig.sendOtpUrl, data);
   }
-
+ 
   verifyOtp(data, config) {
     return this.axiosInstance.post(this.authConfig.verifyOtpUrl, data, config);
   }
-
+ 
   verifyToken() {
     return this.axiosInstance.get(this.authConfig.verifyTokenUrl);
   }
-
+ 
   signUp(credentails) {
     return this.axiosInstance.post(this.authConfig.signUpUrl, credentails);
   }
-
-  signIn(credentails) {
-    return this.axiosInstance.post(this.authConfig.signInUrl, credentails);
-  }
-
+ 
+  // signIn(credentails) {
+  //   return this.axiosInstance.post(this.authConfig.signInUrl, credentails);
+  // }
+ 
   signOut() {
     return this.axiosInstance.post(this.authConfig.signOutUrl);
   }
-
+ 
   async signIn(email, password) {
     // const apiUrl = "http://172.16.47.81:8701";
     const url = `${API_BASE_URL}/login?username=${email}&password=${password}`;
@@ -76,5 +76,5 @@ class AuthApi extends BaseApi {
     }
   }
 }
-
+ 
 export const authApi = new AuthApi();
